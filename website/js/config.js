9b1e2f3a88ffb870ad066927d571c648ae9640d0
Truong <qtruon@f8ffc20c1aee.ant.amazon.com>
2020-03-13 18:28:03 -0700

    enhance and add Pipeline-031320

--- website/js/config.js
+++ website/js/config.js
@@ -3,5 +3,5 @@ var configConstants = {
         domain: 'qtruong-dev.auth0.com',
         clientId: '8BaGhySkwLpcHzqTlxjZ9Zt7LTgjurkr'
     },
-    apiBaseUrl: 'https://n12fqtfhuf.execute-api.us-west-1.amazonaws.com/dev'
+    apiBaseUrl: 'https://as6bct0z0k.execute-api.us-west-1.amazonaws.com/dev'
 };
